03a3013f2bb80958a8318d3ec65d565c1a887d14
Andrei Rusu <andreirusu@deepmind.com>
2013-04-27 18:36:42 +0100

    Refactored and debugged whitening.

--- dataset/whitening.lua
+++ dataset/whitening.lua
@@ -4,22 +4,30 @@ require 'dataset'
 
 
 -- ZCA-Whitening
-function dataset.zca_whiten(data)
-
-	local dims = data:size()
-	local nsamples = dims[1]
-	local n_dimensions = dims[2] * dims[3] * dims[4]
-
-	local mdata = data:clone():reshape(nsamples, n_dimensions)
-	mdata:add(torch.ger(torch.ones(nsamples), torch.mean(mdata, 1):squeeze()):mul(-1))
-	local ce, cv = unsup.pcacov(data:reshape(nsamples, n_dimensions))
-	local val = ce:clone():add(1e-1):sqrt():pow(-1)
-	local diag = torch.diag(val)
-	local P = torch.mm(cv, diag)
-	P = torch.mm(P, cv:t())
-	local wdata = torch.mm(mdata, P)
-	local mwdata = wdata:clone():add(torch.ger(torch.ones(nsamples), torch.mean(wdata, 1):squeeze()):mul(-1))
-	data:copy(wdata:reshape(dims[1], dims[2], dims[3], dims[4]):typeAs(data))
-	return torch.mean(mdata, 1), P
+function dataset.zca_whiten(data, means, P)
+	local auxdata = data:clone()
+    local dims = data:size()
+    local nsamples = dims[1]
+    local n_dimensions = data:nElement() / nsamples
+    if data:dim() >= 3 then
+        auxdata:resize(nsamples, n_dimensions)
+    end
+    if not means and not P then
+        -- compute mean vector if not provided 
+        means = torch.mean(auxdata, 1):squeeze()
+        -- compute transformation matrix P if not provided
+        local ce, cv = unsup.pcacov(auxdata)
+        ce:add(1e-5):sqrt():pow(-1)
+        local diag = torch.diag(ce)
+	    P = torch.mm(cv, diag)
+	    P = torch.mm(P, cv:t())
+    end
+    -- remove the means
+	auxdata:add(torch.ger(torch.ones(nsamples), means):mul(-1))
+    -- rotate in ZCA space
+	auxdata = torch.mm(auxdata, P)
+	
+    data:copy(auxdata:resizeAs(data))
+	return means, P
 end
 
